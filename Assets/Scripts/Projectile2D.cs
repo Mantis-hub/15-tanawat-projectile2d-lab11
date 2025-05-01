6d060cab9514816eda85d634d0bce9f844cf3024
Mantis-hub <tanawat.panyac@bumail.net>
2025-05-01 19:00:20 +0700

    Add Scripts projectile2D, add prefabs

---
+++ Assets/Scripts/Projectile2D.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Projectile2D : MonoBehaviour
+{
+    [SerializeField] private Transform ShootPoint;
+    [SerializeField] GameObject Target;
+    [SerializeField] private Rigidbody2D BulletPrefabs;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
