e66cf45787f66129b48fe9f10fabbd0ad1ff7a48
Mantis-hub <tanawat.panyac@bumail.net>
2025-05-01 19:36:38 +0700

    Update scripts projectile2d

--- Assets/Scripts/Projectile2D.cs
+++ Assets/Scripts/Projectile2D.cs
@@ -9,6 +9,35 @@ public class Projectile2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+            
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                Target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log("hit " + hit.collider.name);
+                
+                Vector2 projectileVelocity = CalculateProjectileVelocity(ShootPoint.position, hit.point, 1f);
+                
+                Rigidbody2D shootBullet = Instantiate(BulletPrefabs, ShootPoint.position, Quaternion.identity);
+
+                shootBullet.linearVelocity = projectileVelocity;
+            }
+        }
+    }
+
+    Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+    {
+        Vector2 distance = target - origin;
+
+        float velocityx = distance.x / time;
+        float velocityy = distance.y / time +0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
         
+        Vector2 projectileVelocity = new Vector2(velocityx, velocityy);
+        return projectileVelocity;
     }
 }
